ea291fb0d9dc0248a1187416ccf6fd3cd6e0f315
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-04-05 17:24:01 -0300

    validacoes na criacao do usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,9 +3,28 @@ const User = require("../models/User")
 module.exports = class UserController {
 
     static async register(req, res) {
+
+        const { name, email, phone, password, confirmPassword } = req.body
+
+        //validations
+        if(!name || !email|| !phone || !password || !confirmPassword){
+            return res.status(422).json({ message: "Todos os campos obrigatórios devem ser preeenchidos!"})
+            
+        }
+        if(password !== confirmPassword){
+            return res.status(422).json({ message: "Senhas não coincidem!"})
+        }
+        
+        //check if user exists
+        const userExists = await User.findOne({email: email})
+        if(userExists){
+            return res.status(422).json({ message: "Email já Existe!"})
+        }
+
+        
         return res.json({
             error: false,
-            message: "good"
+            message: "Cadastred com sucesso"
         })
     }
 }
