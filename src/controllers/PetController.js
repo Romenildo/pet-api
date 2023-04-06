87aba1d659f1f3ddcd92aaad56e99d14b9197296
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-04-06 20:18:34 -0300

    get all Pets por quem adotou

--- src/controllers/PetController.js
+++ src/controllers/PetController.js
@@ -80,5 +80,21 @@ module.exports = class PetController {
         })
     }
 
+    static async getAllUserAdoptions(req, res){
+
+        //get user
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pets = await Pet.find({'adopter._id': user._id}).sort("-createdAt")
+        res.status(200).json({
+            pets: pets
+        })
+
+        res.status(200).json({
+            pets
+        })
+    }
+
 
 }
